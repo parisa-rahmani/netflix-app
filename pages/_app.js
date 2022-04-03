d599b1d5d8e4c4547bb29f1b31ba82df3fdbe437
parisa-rahmani <parisa.rahmani998@gmail.com>
2022-04-03 22:07:34 +0430

    add conditional routing based on user login status

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
 import '../styles/globals.css';
+import { useEffect, useState } from 'react';
+import { magic } from '../utils/magic-client';
+import { useRouter } from 'next/router';
+import Loading from '../components/loading/loading';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const getLoginStat = async () => {
+            const isLoggedIn = await magic.user.isLoggedIn();
+            if (isLoggedIn) {
+                router.push('/');
+            } else {
+                router.push('/login');
+            }
+        };
+        getLoginStat();
+    }, []);
+
+    useEffect(() => {
+        const handleComplete = () => {
+            setIsLoading(false);
+        };
+        router.events.on('routeChangeComplete', handleComplete);
+        router.events.on('routeChangeError', handleComplete);
+
+        return () => {
+            router.events.off('routeChangeComplete', handleComplete);
+            router.events.off('routeChangeError', handleComplete);
+        };
+    }, [router]);
+
+    return isLoading ? <Loading /> : <Component {...pageProps} />;
 }
 
 export default MyApp;
