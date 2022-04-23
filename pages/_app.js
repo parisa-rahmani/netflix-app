0afcc068fa6706af004486536d24da0785b364dc
parisa-rahmani <parisa.rahmani998@gmail.com>
2022-04-23 14:32:19 +0430

    improve router loading state

--- pages/_app.js
+++ pages/_app.js
@@ -9,23 +9,30 @@ function MyApp({ Component, pageProps }) {
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        const handleStart = () => {
+            setIsLoading(true);
+        };
         const handleComplete = () => {
             setIsLoading(false);
         };
+        router.events.on('routeChangeStart', handleStart);
         router.events.on('routeChangeComplete', handleComplete);
         router.events.on('routeChangeError', handleComplete);
 
         return () => {
+            router.events.off('routeChangeStart', handleStart);
             router.events.off('routeChangeComplete', handleComplete);
             router.events.off('routeChangeError', handleComplete);
         };
     }, [router]);
 
-    return isLoading ? (
-        <Loading />
-    ) : (
+    return (
         <AnimatePresence exitBeforeEnter>
-            <Component {...pageProps} key={router.route} />
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <Component {...pageProps} key={router.route} />
+            )}
         </AnimatePresence>
     );
 }
